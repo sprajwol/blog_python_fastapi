dc3e96a0a6e3e8d6fa3ebd1d86324da6775f1929
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-24 12:59:23 +0545

    login process

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import psycopg2
-from fastapi import FastAPI, APIRouter
-from random import randrange
+from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
 
 from app.database import engine
 from app import models
-from app.routers import post, user
+from app.routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,38 +16,39 @@ app = FastAPI()
 # requests GET method url: "/"
 
 
-while True:
-    try:
-        conn = psycopg2.connect(
-            host='localhost', database='fastapi', user='postgres', password='postgres', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database Connection was successful.")
-        break
-    except Exception as error:
-        # time.sleep(2)
-        print("Connecting to database failed.")
-        print(f"Error: {error}")
+# while True:
+#     try:
+#         conn = psycopg2.connect(
+#             host='localhost', database='fastapi', user='postgres', password='postgres', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database Connection was successful.")
+#         break
+#     except Exception as error:
+#         # time.sleep(2)
+#         print("Connecting to database failed.")
+#         print(f"Error: {error}")
 
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2}
-]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
+# my_posts = [
+#     {"title": "title of post 1", "content": "content of post 1", "id": 1},
+#     {"title": "title of post 2", "content": "content of post 2", "id": 2}
+# ]
 
 
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+# def find_post(id):
+#     for p in my_posts:
+#         if p['id'] == id:
+#             return p
+
+
+# def find_index_post(id):
+#     for i, p in enumerate(my_posts):
+#         if p['id'] == id:
+#             return i
 
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get('/')
--- app/routers/post.py
+++ app/routers/post.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app import models, schemas
-from app.database import engine, get_db
+from app.database import get_db
 
 router = APIRouter(
     prefix="/posts",
