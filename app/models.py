05df7a071d3fe48cfff01042febfdbdf2a048f70
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-25 10:58:31 +0545

    vote models

--- app/models.py
+++ app/models.py
@@ -15,9 +15,11 @@ class Post(Base):
     published = Column(Boolean, server_default='True')
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User")
+
 
 class User(Base):
     __tablename__ = "users"
@@ -27,3 +29,12 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey("users.id",
+                     ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id",
+                     ondelete="CASCADE"), primary_key=True)
