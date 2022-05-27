eecc9d4392f6e1dd37bcdbd33aebff92a2eb6af6
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-27 12:28:43 +0545

    sql join in get one post

--- app/routers/post.py
+++ app/routers/post.py
@@ -19,12 +19,11 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # )
     # posts = cursor.fetchall()
 
-    # posts = db.query(models.Post).filter(
-    #     models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-    return results
+    return posts
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -35,6 +34,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     # )
     # new_post = cursor.fetchone()
     # conn.commit()
+
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
@@ -43,14 +43,16 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     return new_post
 
 
-@router.get('/{id}', response_model=schemas.Post)
+@router.get('/{id}', response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(
     #     """SELECT * FROM posts WHERE id = %s""",
     #     (str(id),)
     # )
     # post = cursor.fetchone()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post_query = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id)
     post = post_query.first()
 
     if not post:
@@ -67,6 +69,7 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     # )
     # deleted_post = cursor.fetchone()
     # conn.commit()
+
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
